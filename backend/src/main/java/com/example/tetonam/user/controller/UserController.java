b15451c65b8a374e637d2837f2f8d6c6c716ef19
jjaewooks <jj99526@naver.com>
2025-07-23 17:21:46 +0900

    feat : email,nickname 중복체크 API

--- backend/src/main/java/com/example/tetonam/user/controller/UserController.java
+++ backend/src/main/java/com/example/tetonam/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.tetonam.user.controller;
 
 
-
 import com.example.tetonam.user.domain.JwtToken;
 import com.example.tetonam.user.dto.UserDto;
 import com.example.tetonam.user.dto.ReissueDto;
@@ -21,63 +20,91 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 public class UserController {
 
-  private final UserService userService;
+    private final UserService userService;
 
-  private final JwtTokenProvider jwtTokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
-  @GetMapping("/my-nickname")
-  public ResponseEntity<?> findNickname(@RequestHeader("Authorization") String token) {
-    String email=jwtTokenProvider.getUserName(token.substring(7));
-    String nickname= userService.findByNickname(email);
-    return ResponseEntity.ok().body(ApiResponse.onSuccess(nickname));
-  }
+    /**
+     * 내 닉네임 보기
+     *
+     * @param token
+     * @return
+     */
+    @GetMapping("/my-nickname")
+    public ResponseEntity<?> findNickname(@RequestHeader("Authorization") String token) {
+        String email = jwtTokenProvider.getUserName(token.substring(7));
+        String nickname = userService.findByNickname(email);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(nickname));
+    }
 
-  @PostMapping("/email-duplicate-check")
-  public ResponseEntity<?> checkEmailDuplicate(@RequestParam("email") String email) {
-    String result=userService.checkEmailDuplicate(email);
-    return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
-  }
+    /**
+     * 이메일 중복체크
+     *
+     * @param email
+     * @return
+     */
+    @PostMapping("/email-duplicate-check")
+    public ResponseEntity<?> checkEmailDuplicate(@RequestParam("email") String email) {
+        String result = userService.checkEmailDuplicate(email);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
+    }
 
-  @PostMapping("/nickname-duplicate-check")
-  public ResponseEntity<?> checknicknameDuplicate(@RequestParam("nickname") String nickname) {
-    String result=userService.checknicknameDuplicate(nickname);
-    return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
-  }
+    /**
+     * 닉네임 중복체크
+     *
+     * @param nickname
+     * @return
+     */
+    @PostMapping("/nickname-duplicate-check")
+    public ResponseEntity<?> checknicknameDuplicate(@RequestParam("nickname") String nickname) {
+        String result = userService.checknicknameDuplicate(nickname);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
+    }
 
-  @PostMapping("/sign-in")
-  public ResponseEntity<?> signIn(@RequestBody SignInDto signInDto) {
-    String email = signInDto.getEmail();
-    String password = signInDto.getPassword();
+    /**
+     * 로그인
+     *
+     * @param signInDto
+     * @return
+     */
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> signIn(@RequestBody SignInDto signInDto) {
+        String email = signInDto.getEmail();
+        String password = signInDto.getPassword();
+        JwtToken jwtToken = userService.signIn(email, password);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(jwtToken));
+    }
 
-    // 로그인 처리
-    JwtToken jwtToken = userService.signIn(email, password);
+    /**
+     * 회원가입
+     *
+     * @param signUpDto
+     * @return
+     */
+    @PostMapping("/sign-up")
+    public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto) {
+        // 회원가입 처리
+        UserDto savedMemberDto = userService.signUp(signUpDto);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(savedMemberDto));
+    }
 
-    return ResponseEntity.ok().body(ApiResponse.onSuccess(jwtToken));
-  }
+    /**
+     * 토큰 재발급
+     *
+     * @param reissueDto
+     * @return
+     */
+    @PostMapping("/token/reissue")
+    public ResponseEntity<?> reissue(@RequestBody ReissueDto reissueDto) {
+        // 토큰 재발급 처리
+        JwtToken jwtToken = userService.reissue(reissueDto);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(jwtToken));
+    }
+//    @PatchMapping("/password")
+//    public ResponseEntity<?> resetPassword(@RequestBody SignInDto signInDto) {
+//        // 토큰 재발급 처리
+//        JwtToken jwtToken = userService.reissue(reissueDto);
+//        return ResponseEntity.ok().body(ApiResponse.onSuccess(jwtToken));
+//    }
 
-  @PostMapping("/sign-up")
-  public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto) {
-    log.info("[signUp] 회원가입 요청: username = {}, nickname = {}", signUpDto.getEmail(), signUpDto.getNickname());
-
-    // 회원가입 처리
-    UserDto savedMemberDto = userService.signUp(signUpDto);
-
-    // 회원가입 성공 로그
-    log.info("[signUp] 회원가입 성공: username = {}", signUpDto.getEmail());
-
-    return ResponseEntity.ok().body(ApiResponse.onSuccess(savedMemberDto));
-  }
-
-  @PostMapping("/token/reissue")
-  public ResponseEntity<?> reissue(@RequestBody ReissueDto reissueDto) {
-    log.info("[reissue] 토큰 재발급 요청: accessToken = {}", reissueDto.getAccessToken());
-
-    // 토큰 재발급 처리
-    JwtToken jwtToken = userService.reissue(reissueDto);
-
-    // 토큰 재발급 성공 로그
-    log.info("[reissue] 토큰 재발급 성공: accessToken = {}, refreshToken = {}", jwtToken.getAccessToken(), jwtToken.getRefreshToken());
-
-    return ResponseEntity.ok().body(ApiResponse.onSuccess(jwtToken));
-  }
 }
