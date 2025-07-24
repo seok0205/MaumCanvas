df9cf0b52fe7392aa7c2b8c04af65908cafdb461
jjaewooks <jj99526@naver.com>
2025-07-24 17:27:08 +0900

    feat : 설문 결과 조회

--- backend/src/main/java/com/example/tetonam/diagnosis/controller/SurveyController.java
+++ backend/src/main/java/com/example/tetonam/diagnosis/controller/SurveyController.java
@@ -1,12 +1,16 @@
 package com.example.tetonam.diagnosis.controller;
 
 import com.example.tetonam.diagnosis.domain.enums.Category;
+import com.example.tetonam.diagnosis.dto.ShowAllQuestionnaireDto;
+import com.example.tetonam.diagnosis.dto.ShowCategoryQuestionnaireDto;
 import com.example.tetonam.diagnosis.service.SurveyService;
 import com.example.tetonam.response.ApiResponse;
 import com.example.tetonam.user.token.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,9 +22,23 @@ public class SurveyController {
 
 
     @PostMapping("/questionnaire")
-    public ResponseEntity<?> stressCreate(@RequestHeader("Authorization") String token, @RequestParam("score") int score,@RequestParam("category") Category category) {
+    public ResponseEntity<?> createQuestionnaire(@RequestHeader("Authorization") String token, @RequestParam("score") int score, @RequestParam("category") Category category) {
         String email = jwtTokenProvider.getEmail(token.substring(7));
-        String result=surveyService.stressCreate(email,score,category);
+        String result=surveyService.questionnaireCreate(email,score,category);
         return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
     }
+    @GetMapping("/questionnaire")
+    public ResponseEntity<?> showQuestionnaire(@RequestHeader("Authorization") String token) {
+        String email = jwtTokenProvider.getEmail(token.substring(7));
+        List<ShowAllQuestionnaireDto> result=surveyService.showQuestionnaire(email);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
+    }
+
+    @GetMapping("/questionnaire/{category}")
+    public ResponseEntity<?> showCategoryQuestionnaire(@RequestHeader("Authorization") String token,@PathVariable("category") Category category) {
+        String email = jwtTokenProvider.getEmail(token.substring(7));
+        List<ShowCategoryQuestionnaireDto> result=surveyService.showCategoryQuestionnaire(email,category);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
+    }
+
 }
--- backend/src/main/java/com/example/tetonam/diagnosis/service/SurveyService.java
+++ backend/src/main/java/com/example/tetonam/diagnosis/service/SurveyService.java
@@ -3,6 +3,8 @@ package com.example.tetonam.diagnosis.service;
 
 import com.example.tetonam.diagnosis.domain.Survey;
 import com.example.tetonam.diagnosis.domain.enums.Category;
+import com.example.tetonam.diagnosis.dto.ShowAllQuestionnaireDto;
+import com.example.tetonam.diagnosis.dto.ShowCategoryQuestionnaireDto;
 import com.example.tetonam.diagnosis.repository.SurveyRepository;
 import com.example.tetonam.exception.handler.UserHandler;
 import com.example.tetonam.response.code.status.ErrorStatus;
@@ -11,16 +13,44 @@ import com.example.tetonam.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class SurveyService {
     private final SurveyRepository surveyRepository;
     private final UserRepository userRepository;
-    public String stressCreate(String email, int score, Category category) {
+    public String questionnaireCreate(String email, int score, Category category) {
         User user=userRepository.findByEmail(email)
                 .orElseThrow(() -> new UserHandler(ErrorStatus.USER_NOT_FOUND));
         Survey survey=Survey.builder().score(score).user(user).category(category).build();
         surveyRepository.save(survey);
         return "설문결과가 저장되었습니다.";
+    }
+
+    public List<ShowAllQuestionnaireDto> showQuestionnaire(String email) {
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserHandler(ErrorStatus.USER_NOT_FOUND));
+
+        List<ShowAllQuestionnaireDto> showAllQuestionnaireDtoList=new ArrayList<>();
+        for(Category c:Category.values()){
+            Survey survey=surveyRepository.findTopByUserIdAndCategoryOrderByCreatedAtDesc(user,c);
+            if(survey==null) {
+                showAllQuestionnaireDtoList.add(new ShowAllQuestionnaireDto(c, -1));
+                continue;
+            }
+            showAllQuestionnaireDtoList.add(new ShowAllQuestionnaireDto(c,survey.getScore()));
+        }
+
+        return showAllQuestionnaireDtoList;
+    }
+
+    public List<ShowCategoryQuestionnaireDto> showCategoryQuestionnaire(String email, Category category) {
+        User user=userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserHandler(ErrorStatus.USER_NOT_FOUND));
+        List<Survey> surveyList=surveyRepository.findByCategoryAndUser(user,category);
+        return surveyList.stream().map(ShowCategoryQuestionnaireDto::toDto).collect(Collectors.toList());
     }
 }
