fb2747bb0400336a4fab0cd4642323053a909585
김현서 <rjdls2549@gmail.com>
2025-07-25 15:29:19 +0900

    <AI> feat : reqirements 리눅스 환경 맞춰 작성 및 predict model json/image return api 이식 완료

--- AI/app/main.py
+++ AI/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from app.routers import example
+from app.routers import example, predict
 
 app = FastAPI()
 
 app.include_router(example.router)
+app.include_router(predict.router)
