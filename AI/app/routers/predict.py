1008b4ebad28f4c88f0b6995a08e6d4efad19dbc
김현서 <rjdls2549@gmail.com>
2025-07-30 12:37:51 +0900

    <AI> feat : ai result to String YOLO v5 모델 분석 결과 -> 활용 가능한 자연어 문장으로 변환 Filter api 사용 시 세부 요소 무시되는 경우 있어서 우선 filter 제거

--- AI/app/routers/predict.py
+++ AI/app/routers/predict.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, File, UploadFile
 from fastapi.responses import JSONResponse, StreamingResponse
 from app.core.yolo_predict import run_inference
 from app.utils.filtering import nms_filter
+from app.utils.calcSize import calc
+from app.utils.toString import toString
 import numpy as np
 import cv2
 import io
@@ -17,8 +19,11 @@ from app.utils.s3imageLoader import s3imageLoader
 async def predict_json_s3(url):
     img_np = s3imageLoader(url)
     _, results = run_inference(img_np, model, class_names, visualize=False)
-    filtered_result = nms_filter(results)
-    return {"num_detections": len(filtered_result), "detections": filtered_result}
+    # 일단 필터 빼
+    # filtered_result = nms_filter(results)
+    calc_result = calc(results)
+    stringVal = toString(calc_result)
+    return stringVal
 
 ###########################################################################
 
@@ -28,8 +33,12 @@ async def predict_json(file: UploadFile = File(...)):
     image_bytes = await file.read()
     img_np = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
     _, results = run_inference(img_np, model, class_names, visualize=False)
-    filtered_result = nms_filter(results)
-    return {"num_detections": len(filtered_result), "detections": filtered_result}
+    # 일단 필터 빼
+    # filtered_result = nms_filter(results)
+    calc_result = calc(results)
+    stringVal = toString(calc_result)
+    # return {"num_detections": len(calc_result), "detections": calc_result}
+    return stringVal
 
 @router.post("/image")
 async def predict_image(file: UploadFile = File(...)):
