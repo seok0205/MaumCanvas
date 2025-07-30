185ab68fed88634d2dffbfd2de1426ad9a5b5e0f
jjaewooks <jj99526@naver.com>
2025-07-30 12:40:18 +0900

    <Back> feat : Mypage에서 비밀번호 수정

--- backend/src/main/java/com/example/tetonam/user/controller/UserController.java
+++ backend/src/main/java/com/example/tetonam/user/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.tetonam.user.dto.*;
 import com.example.tetonam.user.service.UserService;
 import com.example.tetonam.user.token.JwtTokenProvider;
 import com.example.tetonam.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -28,6 +29,7 @@ public class UserController {
      * @return
      */
     @GetMapping("/my-nickname")
+    @Operation(summary = "내 닉네임 확인 API", description = "내 닉네임을 확인합니다.")
     public ResponseEntity<?> findNickname(@RequestHeader("Authorization") String token) {
         String email = jwtTokenProvider.getEmail(token.substring(7));
         String nickname = userService.findByNickname(email);
@@ -41,6 +43,8 @@ public class UserController {
      * @return
      */
     @PostMapping("/email-duplicate-check")
+    @Operation(summary = "이메일 중복확인 API", description = "이메일을 중복을 확인합니다.")
+
     public ResponseEntity<?> checkEmailDuplicate(@RequestParam("email") String email) {
         String result = userService.checkEmailDuplicate(email);
         return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
@@ -53,6 +57,7 @@ public class UserController {
      * @return
      */
     @PostMapping("/nickname-duplicate-check")
+    @Operation(summary = "닉네임 중복확인 API", description = "닉네임 중복을 확인합니다.")
     public ResponseEntity<?> checknicknameDuplicate(@RequestParam("nickname") String nickname) {
         String result = userService.checknicknameDuplicate(nickname);
         return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
@@ -65,6 +70,7 @@ public class UserController {
      * @return
      */
     @PostMapping("/sign-in")
+    @Operation(summary = "로그인 API", description = "로그인을 합니다.")
     public ResponseEntity<?> signIn(@RequestBody SignInDto signInDto) {
         String email = signInDto.getEmail();
         String password = signInDto.getPassword();
@@ -79,6 +85,7 @@ public class UserController {
      * @return
      */
     @PostMapping("/sign-up")
+    @Operation(summary = "회원가입 API", description = "회원가입을 합니다.")
     public ResponseEntity<?> signUp(@RequestBody SignUpDto signUpDto) {
         // 회원가입 처리
         UserDto savedMemberDto = userService.signUp(signUpDto);
@@ -92,12 +99,31 @@ public class UserController {
      * @return
      */
     @PostMapping("/token/reissue")
+    @Operation(summary = "토큰 재발급 API", description = "액세스토큰,리프레쉬토큰을 재발급합니다.(RTR)")
     public ResponseEntity<?> reissue(@RequestBody ReissueDto reissueDto) {
         // 토큰 재발급 처리
         JwtToken jwtToken = userService.reissue(reissueDto);
         return ResponseEntity.ok().body(ApiResponse.onSuccess(jwtToken));
     }
+
+    @PatchMapping("/mypage-password")
+    @Operation(summary = "비밀번호 재설정 API", description = "비밀번호를 재설정합니다.")
+    public ResponseEntity<?> mypageResetPassword(@RequestHeader("Authorization") String token,@RequestParam("password") String password) {
+        String email = jwtTokenProvider.getEmail(token.substring(7));
+        String result=userService.mypageResetPassword(email,password);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
+    }
+
+
+
+    /**
+     * 비밀번호 찾기
+     * @param resetPasswordDto
+     * @return
+     */
     @PatchMapping("/password")
+    @Operation(summary = "비밀번호 찾기 API", description = "비밀번호를 찾습니다.")
+
     public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordDto resetPasswordDto) {
         String result=userService.resetPassword(resetPasswordDto);
         return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
