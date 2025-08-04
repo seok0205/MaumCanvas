6247328836769a63ed47741480c3cc43e8977c57
김현서 <rjdls2549@gmail.com>
2025-08-04 09:47:35 +0900

    <AI> feat : make dummy LLM  API

--- AI/app/main.py
+++ AI/app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from app.routers import example, predict
+from app.routers import example, predict, returnLLM
 
 app = FastAPI()
 
 app.include_router(example.router)
 app.include_router(predict.router)
+app.include_router(returnLLM.router)
