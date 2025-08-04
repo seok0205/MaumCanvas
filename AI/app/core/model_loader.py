be404c33f303da5249b7a0c6c005ad1890bc2855
김현서 <rjdls2549@gmail.com>
2025-08-04 11:52:53 +0900

    <AI> feat : make 3 category and add 3 model

--- AI/app/core/model_loader.py
+++ AI/app/core/model_loader.py
@@ -1,18 +1,14 @@
 from app.models.common import DetectMultiBackend
 
-weights_path = "app/weights/best.pt"
 device = "cpu"
 
-model = DetectMultiBackend(weights_path, device=device)
-model.eval()
+houseModel = DetectMultiBackend("app/weights/house.pt", device=device)
+personModel = DetectMultiBackend("app/weights/person.pt", device=device)
+treeModel = DetectMultiBackend("app/weights/tree.pt", device=device)
+houseModel.eval()
+personModel.eval()
+treeModel.eval()
+class_names_house = ['집전체', '지붕', '집벽', '문', '창문', '굴뚝', '연기', '울타리', '길', '연못', '산', '잔디', '꽃', '태양', '기타_집']
+class_names_tree = ['나무전체', '기둥', '수관', '가지', '뿌리', '나뭇잎', '나무꽃', '열매', '그네', '새', '다람쥐', '구름', '달', '별']
+class_names_person = ['사람전체', '머리', '얼굴', '눈', '코', '입', '귀', '머리카락', '목', '상체', '팔', '손', '다리', '발', '단추', '주머니', '운동화', '구두']
 
-class_names = [
-    # 집 (15)
-    '집전체', '지붕', '집벽', '문', '창문', '굴뚝', '연기', '울타리', '길', '연못', '산', '잔디', '꽃', '태양', '기타_집',
-    # 나무 (14)
-    '나무전체', '기둥', '수관', '가지', '뿌리', '나뭇잎', '나무꽃', '열매', '그네', '새', '다람쥐', '구름', '달', '별',
-    # 여자사람 (18)
-    '여자전체', '머리', '얼굴', '눈', '코', '입', '귀', '머리카락', '목', '상체', '팔', '손', '다리', '발', '단추', '주머니', '운동화', '여자구두',
-    # 남자사람 (18)
-    '남자전체', '머리', '얼굴', '눈', '코', '입', '귀', '머리카락', '목', '상체', '팔', '손', '다리', '발', '단추', '주머니', '운동화', '남자구두'
-]
--- AI/app/routers/predict.py
+++ AI/app/routers/predict.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Form
 from fastapi.responses import JSONResponse, StreamingResponse
 from app.core.yolo_predict import run_inference
 from app.utils.filtering import nms_filter
@@ -11,12 +11,25 @@ import io
 router = APIRouter(prefix="/predict", tags=["Prediction"])
 
 # 미리 로드된 전역 모델 및 클래스
-from app.core.model_loader import model, class_names
+from app.core.model_loader import personModel, treeModel, houseModel, class_names_house, class_names_person, class_names_tree
 from app.utils.s3imageLoader import s3imageLoader
+#모든 카테고리는 person, tree, house로 고정입니다
+
+
 
 #이미지 s3로 받을 때
 @router.post("/json_s3")
-async def predict_json_s3(url):
+async def predict_json_s3(url, category):
+    if category == "person":
+        model = personModel
+        class_names = class_names_person
+    elif category == "tree":
+        model = treeModel
+        class_names = class_names_tree
+    else:
+        model = houseModel
+        class_names = class_names_house
+
     img_np = s3imageLoader(url)
     _, results = run_inference(img_np, model, class_names, visualize=False)
     # 일단 필터 빼
@@ -29,7 +42,16 @@ async def predict_json_s3(url):
 
 #이미지 업로드로 받을 때
 @router.post("/json")
-async def predict_json(file: UploadFile = File(...)):
+async def predict_json(file: UploadFile = File(...), category: str = Form(...)):
+    if category == "person":
+        model = personModel
+        class_names = class_names_person
+    elif category == "tree":
+        model = treeModel
+        class_names = class_names_tree
+    else:
+        model = houseModel
+        class_names = class_names_house
     image_bytes = await file.read()
     img_np = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
     _, results = run_inference(img_np, model, class_names, visualize=False)
@@ -41,7 +63,16 @@ async def predict_json(file: UploadFile = File(...)):
     return stringVal
 
 @router.post("/image")
-async def predict_image(file: UploadFile = File(...)):
+async def predict_image(file: UploadFile = File(...), category: str = Form(...)):
+    if category == "person":
+        model = personModel
+        class_names = class_names_person
+    elif category == "tree":
+        model = treeModel
+        class_names = class_names_tree
+    else:
+        model = houseModel
+        class_names = class_names_house
     image_bytes = await file.read()
     img_np = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), cv2.IMREAD_COLOR)
     img_result, _ = run_inference(img_np, model, class_names, visualize=True)
