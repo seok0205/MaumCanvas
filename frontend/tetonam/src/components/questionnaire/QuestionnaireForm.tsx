83fb5f9b9d72585cb9cf2d514350fc645570dfb4
Hyeseong Ro <nks3319@gmail.com>
2025-08-06 04:06:57 +0900

    <FE> feat: QuestionnaireForm 컴포넌트 추가
    - 설문조사 폼 컴포넌트 구현
    - 간단한 테스트용 UI 구성 및 텍스트 추가

---
+++ frontend/tetonam/src/components/questionnaire/QuestionnaireForm.tsx
@@ -0,0 +1,8 @@
+export const QuestionnaireForm = () => {
+  return (
+    <div>
+      <h1>QuestionnaireForm Component - Working Version</h1>
+      <p>This is a simplified version for testing.</p>
+    </div>
+  );
+};
