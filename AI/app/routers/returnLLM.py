074874088d4cc457fe38047a4f943716093f60b6
박진호 <dodwn48@gmail.com>
2025-08-06 11:32:34 +0900

    <AI>feat: endpoint 로직 추가

--- AI/app/routers/returnLLM.py
+++ AI/app/routers/returnLLM.py
@@ -1,10 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from app.schemas.schema import AnalysisResponse, AnalysisRequest
+from app.rag.state import GraphState
 
 router = APIRouter(prefix="/llm", tags=["llm"])
 
+
 @router.post("/llm")
-async def return_LLM(text):
-    if text:
-        return "연결 성공"
-    else :
-        return "연결 실패"
+async def return_LLM(request_body: AnalysisRequest, request: Request):
+    """
+    LLM을 호출하여 분석 요청에 대한 응답을 반환합니다.
+    """
+    rag_graph = request.app.state.rag_graph
+
+    inputs = GraphState(
+        question=request_body.question,
+        category=request_body.category,
+    )
+
+    final_state = rag_graph.invoke(inputs, {"recursion_limit": 15})
+
+    return AnalysisResponse(
+        answer=final_state.get("generation", "해석을 생성하지 못했습니다."),
+    )
