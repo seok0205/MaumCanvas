17c589b9749f65d15a77052371cc6b42b85e1a02
김현서 <rjdls2549@gmail.com>
2025-08-06 11:32:47 +0900

    <BE> feat : 댓글 수정 api

--- backend/src/main/java/com/example/tetonam/community/controller/CommentController.java
+++ backend/src/main/java/com/example/tetonam/community/controller/CommentController.java
@@ -1,11 +1,13 @@
 package com.example.tetonam.community.controller;
 
+import com.example.tetonam.community.domain.Comment;
 import com.example.tetonam.community.domain.Community;
 import com.example.tetonam.community.dto.CommentWriteDto;
 import com.example.tetonam.community.dto.PostListDto;
 import com.example.tetonam.community.dto.PostUpdateDto;
 import com.example.tetonam.community.dto.PostWriteDto;
 import com.example.tetonam.community.repository.CommunityRepository;
+import com.example.tetonam.community.repository.CommentRepository;
 import com.example.tetonam.community.service.CommentService;
 import com.example.tetonam.community.service.CommunityService;
 import com.example.tetonam.user.domain.User;
@@ -31,6 +33,7 @@ public class CommentController {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserRepository userRepository;
     private final CommunityRepository communityRepository;
+    private final CommentRepository commentRepository;
 
     /**
      * 게시글 단건 조회
@@ -69,16 +72,16 @@ public class CommentController {
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "글 수정 API", description = "등록된 글을 수정합니다")
-    public ResponseEntity<PostUpdateDto> updatePost(@PathVariable Long id, @RequestBody PostUpdateDto updatedCommunity, @RequestHeader("Authorization") String token){
+    @Operation(summary = "댓글 수정 API", description = "등록된 댓글을 수정합니다")
+    public ResponseEntity<CommentWriteDto> updatePost(@PathVariable Long community_id, @PathVariable Long id, @RequestBody CommentWriteDto updatedComment, @RequestHeader("Authorization") String token){
         String jwt = token.substring(7);
         String email = jwtTokenProvider.getEmail(jwt);
-        Community community = communityRepository.findById(id)
+        Comment comment = commentRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "유효하지 않은 게시글"));
-        if(email.equals(community.getAuthor().getEmail())){
-            Community updateCommunity = communityService.updatePost(id, updatedCommunity);
+        if(email.equals(comment.getAuthor().getEmail())){
+            Comment updateComment = commentService.updateComment(id, updatedComment);
         }
-        return ResponseEntity.ok(updatedCommunity);
+        return ResponseEntity.ok(updatedComment);
     }
 
     @GetMapping
