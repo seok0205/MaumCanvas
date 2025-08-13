e33c9d0d787eb26297d8659454d767d5e205a6e1
jjaewooks <jj99526@naver.com>
2025-08-13 17:16:21 +0900

    <Back> feat : 서버의 시간 확인 (누가 죄인인가)

--- backend/src/main/java/com/example/tetonam/counseling/dto/CounselingReserveRequestDto.java
+++ backend/src/main/java/com/example/tetonam/counseling/dto/CounselingReserveRequestDto.java
@@ -3,6 +3,7 @@ package com.example.tetonam.counseling.dto;
 import com.example.tetonam.counseling.domain.Counseling;
 import com.example.tetonam.counseling.domain.enums.Status;
 import com.example.tetonam.user.domain.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,8 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @Builder
 public class CounselingReserveRequestDto {
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime time;
     private String types;
     private Long counselorId;
