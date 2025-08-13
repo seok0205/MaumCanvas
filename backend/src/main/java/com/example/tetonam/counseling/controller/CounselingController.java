2f21115e9b9c7930e9faef99707da1e091f5e3d7
jjaewooks <jj99526@naver.com>
2025-08-13 16:21:45 +0900

    <Back> feat : 서버의 시간 확인

--- backend/src/main/java/com/example/tetonam/counseling/controller/CounselingController.java
+++ backend/src/main/java/com/example/tetonam/counseling/controller/CounselingController.java
@@ -44,7 +44,7 @@ public class CounselingController {
     @Operation(summary = "상담예약 API", description = "상담사를 선택하여 상담예약을 진행합니다.")
     public ResponseEntity<?> createCounseling(@RequestHeader("Authorization") String token, @RequestBody CounselingReserveRequestDto counselingReserveRequestDto) {
         String email = jwtTokenProvider.getEmail(token.substring(7));
-        System.out.println(counselingReserveRequestDto.getTime());
+        System.out.println("프론트에서 어떻게 시간주는지 확인 합니다!!!!"+counselingReserveRequestDto.getTime());
 //        String result=counselingService.createCounseling(email,counselingReserveRequestDto);
         String lockKey="counselor:" + counselingReserveRequestDto.getCounselorId() + ":time:" + counselingReserveRequestDto.getTime().format(FORMATTER);
         String result=counselingService.createCounselingWithLock(email,counselingReserveRequestDto,lockKey);
